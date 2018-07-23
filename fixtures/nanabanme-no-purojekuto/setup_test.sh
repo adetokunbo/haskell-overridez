98de51f3f584c85312ef6195ffdc7277546f93eb
Tim Emiola <adetokunbo@users.noreply.github.com>
2018-07-23 14:18:45 +0900

    Build turtle-1.5.8 in the integration tests
    - this is the best version to build at the pinned version of nixpkgs

--- fixtures/nanabanme-no-purojekuto/setup_test.sh
+++ fixtures/nanabanme-no-purojekuto/setup_test.sh
@@ -8,7 +8,7 @@ setup_test() {
     HOZ_OPTS=dontCheck
     $HOZ_TEST_CMD cabal://optparse-applicative-0.14.2.0
     HOZ_OPTS=doJailbreak
-    $HOZ_TEST_CMD cabal://turtle-1.5.0
+    $HOZ_TEST_CMD cabal://turtle-1.5.8
     $HOZ_TEST_CMD cabal://foldl-1.3.7
     $HOZ_TEST_CMD cabal://managed-1.0.6
 
